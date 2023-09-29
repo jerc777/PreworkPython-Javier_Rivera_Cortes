b35bdd4be2b3554040b2789f69b916e677d27dbb
jerc777 <javieredo.rivera@gmail.com>
2023-09-29 10:36:37 +0200

    Create Ejercicios_Variables-y-Operadores.py

---
+++ Ejercicios/Ejercicios_Variables-y-Operadores.py
@@ -0,0 +1,16 @@
+#1. Ejercicio: Crea una variable llamada nombre y asígnale tu nombre como valor. Luego, imprime la variable.
+nombre = 'Javier'
+print(nombre)
+
+#2. Ejercicio: Crea dos variables, a y b , y asígnales los valores 5 y 10 respectivamente. Luego, imprime la suma de a y b .
+a = 5
+b = 10
+print(a+b)
+
+#3. Ejercicio: Calcula el área de un triángulo con base 10 y altura 5.
+c = a*b
+print(f'El area del triangulo de base {b} y altura {a}, es de: ',c/2)
+
+#4. Ejercicio: Calcula el resto de dividir 17 entre 3.
+d = 17%3
+print('el resto de dividir 17 entre 3 es:',d)
