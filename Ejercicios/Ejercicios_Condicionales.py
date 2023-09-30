daf44bcb3665de8470963df33912719a5444f5c1
jerc777 <javieredo.rivera@gmail.com>
2023-09-30 09:53:36 +0200

    Create Ejercicios_Condicionales.py

---
+++ Ejercicios/Ejercicios_Condicionales.py
@@ -0,0 +1,16 @@
+#1. Ejercicio: Dado un número, imprime si es positivo o negativo.
+
+def par_impar(num):
+  if num < 0:
+    print(f'El número {num} es Negativo')
+
+  elif num == 0:
+    print(f'El número {num} es Cero')
+  elif num > 0:
+    print(f'El número {num} es Positivo')
+
+par_impar(5)
+
+#2. Ejercicio: Dado un número, imprime si es par o impar.
+
+#3. Ejercicio: Dado tres números, encuentra y muestra el mayor de ellos.
